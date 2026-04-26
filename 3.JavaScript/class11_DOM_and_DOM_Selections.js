23045149894376855b26c5765a2b39d0a444b6f9
Tahrim Janin <janintahrim@gmail.com>
2026-04-26 20:45:26 +0600

    acde

---
+++ 3.JavaScript/class11_DOM_and_DOM_Selections.js
@@ -0,0 +1,18 @@
+//DOM = document object model
+//DOM is a programming interface for HTML and XML documents. 
+//It represents the page so that programs can change the document structure, style, and content. The DOM represents the document as nodes and objects; that way, programming languages can interact with the page
+
+   //for of = array ar jonno
+   //form in = object ar jonno
+
+   //html collect jekono vabe access korte pari aita ki muloto array kina?
+   //ans== no ,aita array no , array like object , aikhane for of or for loop chalaite parleo aikhane others exclusive array method chalaite parbo na
+
+
+    //old way == class_name,tag_name,Id-selector
+    //mordern way == querySelector ,querySelectorAll
+     
+  
+ 
+
+//what is bem
